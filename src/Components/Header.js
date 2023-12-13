d8d77b7d5800d04466ebf6e674d42d51acd87110
JOSE ANDRES MAMANI MOLLERICONA <jamm030197@gmail.com>
2023-12-13 02:44:52 -0400

    codigo de pagina1

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,42 +12,44 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-        <img src={logo} alt='logo' className="logo-img"></img>
+    <div class="container-fluid">
+        <Link to="/">
+            <img src={logo} alt='logo' width={60} height={60} className="logo-img"></img>
+        </Link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <Link class="nav-link" aria-current="page" to="/promociones">Promociones</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/promociones">Promociones</Link>
                 </li>
 
                 <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/menupage">Menu</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/menupage">Menu</Link>
                 </li>
 
                 <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/tradicionales">Platos Tradicionales</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/tradicionales">Platos Tradicionales</Link>
                 </li>
                 <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/extranjeros">Platos Extranjeros</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/extranjeros">Platos Extranjeros</Link>
                 </li>
                 <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/videos">Videos</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/videos">Videos</Link>
                 </li>
                 <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/registro">Registro</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/registro">Registro</Link>
                 </li>
                 <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/pedidos">Pedidos</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/pedidos">Pedidos</Link>
                 </li>
                 <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/perfil">Perfil</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/perfil">Perfil</Link>
                 </li>
                 <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/contactos">Contactos</Link>
+                    <Link class="nav-link text-white" aria-current="page" to="/contactos">Contactos</Link>
                 </li>
                 
                 </ul>
